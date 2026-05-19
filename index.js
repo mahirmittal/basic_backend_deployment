3b0514c2ad712602a1d33064dedd6fea526d2052
Mahir Mittal <mahirmittalmm@gmail.com>
2026-05-19 12:47:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/login', (req,res)=>{
+    res.send("this is a login page")
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
